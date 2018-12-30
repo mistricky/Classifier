02d263b8c685e30b1b6d7e9c5bea8bb978644533
HaoDaWang <1306036576@qq.com>
2018-12-30 22:24:56 +0800

    Add getWindow method

--- src/core/windows-manager.ts
+++ src/core/windows-manager.ts
@@ -17,16 +17,20 @@ export class WindowsManager {
     this.windows.set(key, window);
   }
 
-  closeWindow(key: string): boolean {
+  closeWindow(key: string, isClose?: boolean, isCoercive?: boolean): boolean {
     let win = this.windows.get(key);
 
     if (!win) {
       return false;
     }
 
-    win.close();
+    isClose && (isCoercive ? win.destroy() : win.close());
     this.windows.delete(key);
 
     return true;
+  }
+
+  getWindow(name: string): BrowserWindow | undefined {
+    return this.windows.get(name);
   }
 }
