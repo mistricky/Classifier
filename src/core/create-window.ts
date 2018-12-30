dc944fc82d9a767c24c626c8bd7732393c92388b
HaoDaWang <1306036576@qq.com>
2018-12-30 22:23:43 +0800

    Add close window logic

--- src/core/create-window.ts
+++ src/core/create-window.ts
@@ -1,4 +1,5 @@
 import { BrowserWindow, remote } from "electron";
+import { WindowsManager } from "./windows-manager";
 
 export interface WindowSize {
   height: number;
@@ -8,21 +9,28 @@ export interface WindowSize {
 export type WindowCloseHandler = () => void;
 export type LoadType = "url" | "file";
 export type ProcessType = "render" | "main";
+export type WindowLoadedHandler = () => void;
 
 export function createWindow(
   windowSize: WindowSize,
+  windowName: string,
   processType?: ProcessType,
   loadType?: LoadType,
   loadPath?: string,
+  handleWindowLoaded?: WindowLoadedHandler,
   handleWindowClose?: WindowCloseHandler
 ) {
   let options = Object.assign({}, windowSize, {
     show: false
   });
+
+  let windowsManager = WindowsManager.createInstance();
   let win: BrowserWindow | undefined | null =
     !processType || processType === "main"
       ? new BrowserWindow(options)
       : new remote.BrowserWindow(options);
+
+  windowsManager.addWindow(windowName, win);
 
   if (loadType && loadPath) {
     if (loadType === "file") {
@@ -34,12 +42,15 @@ export function createWindow(
 
   win.on("close", () => {
     handleWindowClose && handleWindowClose();
-
+    windowsManager.closeWindow(windowName);
     win = null;
   });
 
   /** 防止页面抖动 */
-  win.on("ready-to-show", () => win!.show());
+  win.on("ready-to-show", () => {
+    win!.show();
+    handleWindowLoaded && handleWindowLoaded();
+  });
 
   return win;
 }
