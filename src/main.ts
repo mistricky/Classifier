27ef6beae1343786d344d65d8e913bef78984981
HaoDaWang <1306036576@qq.com>
2018-12-30 18:21:54 +0800

    Entry

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from "electron";
+import { createWindow } from "./core/create-window";
+import { LOGIN_WINDOW_SIZE, PAGES_PATH } from "./configs";
+import Path from "path";
+
+const loginPage = Path.join(PAGES_PATH, "login.html");
+
+let win: BrowserWindow | undefined | null;
+
+app.on(
+  "ready",
+  () => (win = createWindow(LOGIN_WINDOW_SIZE, "file", loginPage))
+);
+
+app.on("window-all-closed", () => {
+  /** 菜单栏保持激活 */
+  if (process.platform != "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (win === null) {
+    createWindow(LOGIN_WINDOW_SIZE);
+  }
+});
