30cb6ae3b975364a2b66ffe97077dbcbd99474eb
HaoDaWang <1306036576@qq.com>
2018-12-31 16:36:55 +0800

    Add JSON_PATH

--- src/configs/path-config.ts
+++ src/configs/path-config.ts
@@ -1,3 +1,7 @@
 import Path from "path";
 
-export const PAGES_PATH = Path.join(process.cwd(), "public", "pages");
+const ROOT_PATH = process.cwd();
+
+export const PAGES_PATH = Path.join(ROOT_PATH, "public", "pages");
+export const APP_ICONS_PATH = Path.join(ROOT_PATH, "public", "images", "apps");
+export const JSON_PATH = Path.join(ROOT_PATH, "public", "config");
