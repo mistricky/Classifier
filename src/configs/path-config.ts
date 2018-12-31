6bbdb717f4bfdb8a2daec69f6e4b1de7ffe03d50
HaoDaWang <1306036576@qq.com>
2018-12-31 23:33:37 +0800

    Add APP_LIST

--- src/configs/path-config.ts
+++ src/configs/path-config.ts
@@ -5,3 +5,5 @@ const ROOT_PATH = process.cwd();
 export const PAGES_PATH = Path.join(ROOT_PATH, "public", "pages");
 export const APP_ICONS_PATH = Path.join(ROOT_PATH, "public", "images", "apps");
 export const JSON_PATH = Path.join(ROOT_PATH, "public", "config");
+
+export const APP_LIST = "app-list.json";
