c97f8104a156c48a3ca17ff6cf8fb647bdc65173
HaoDaWang <1306036576@qq.com>
2018-12-31 23:33:52 +0800

    Listen get-app event

--- src/main/process-channel.ts
+++ src/main/process-channel.ts
@@ -1,5 +1,7 @@
-import { ipcMain, BrowserWindow } from "electron";
+import { ipcMain, BrowserWindow, Event } from "electron";
 import { WindowsManager } from "../core";
+import { updateJSON, parseToConfig, AppManager } from "../common";
+import { APP_LIST } from "../configs";
 
 ipcMain.on("close-target-window", (_e: Event, windowName: string) => {
   WindowsManager.createInstance().closeWindow(windowName, true);
@@ -9,6 +11,26 @@ ipcMain.on("hidden-target-window", (_e: Event, windowName: string) => {
   (WindowsManager.createInstance().getWindow(
     windowName
   ) as BrowserWindow).hide();
+});
+
+ipcMain.on("save-app-list", () => {
+  updateJSON(
+    APP_LIST,
+    JSON.stringify(parseToConfig(AppManager.createInstance().apps))
+  );
+});
+
+ipcMain.on("add-app", async (e: Event, path: string) => {
+  let app = await AppManager.createInstance().addApp(path);
+
+  e.sender.send("add-app-reply", app);
+});
+
+ipcMain.on("get-apps", (e: Event) => {
+  e.sender.send(
+    "get-apps-reply",
+    parseToConfig(AppManager.createInstance().apps)
+  );
 });
 
 export {};
