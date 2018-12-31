dee4eafe5aaf34d164a512d7505c940ad366a664
HaoDaWang <1306036576@qq.com>
2018-12-31 14:28:49 +0800

    Add typing for file-icon

---
+++ src/typing.d.ts
@@ -0,0 +1,16 @@
+declare module "file-icon" {
+  interface CommonOptions {
+    size: number;
+  }
+
+  interface FileOptions extends CommonOptions {
+    destination: string;
+  }
+
+  interface BufferOptions extends CommonOptions {}
+
+  type InputType = string | number;
+
+  function file(input: InputType, options?: FileOptions): Promise<void>;
+  function buffer(input: InputType, options?: BufferOptions): Promise<Buffer>;
+}
