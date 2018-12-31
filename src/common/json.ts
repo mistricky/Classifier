20da834ffd51305f0f96680ca24394e9707b5a6c
HaoDaWang <1306036576@qq.com>
2018-12-31 16:47:30 +0800

    Add parseToMap method

--- src/common/json.ts
+++ src/common/json.ts
@@ -1,6 +1,11 @@
 import { writeFile } from "../core";
 import * as Path from "path";
 import { JSON_PATH } from "../configs";
+import { App } from "./app-manager";
+
+export interface AppListConfig {
+  [index: string]: App;
+}
 
 export async function updateJSON(fileName: string, content: string) {
   try {
@@ -8,4 +13,15 @@ export async function updateJSON(fileName: string, content: string) {
   } catch (e) {
     throw e;
   }
+}
+
+export async function parseToMap(fileName: string) {
+  let config: AppListConfig = await import(Path.join(JSON_PATH, fileName));
+  let appMap = new Map<string, App>();
+
+  for (let key of Object.keys(config)) {
+    appMap.set(key, config[key]);
+  }
+
+  return appMap;
 }
