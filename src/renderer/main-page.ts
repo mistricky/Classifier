89206705e1e1e2ea39c6d935467197c2df6f39ad
HaoDaWang <1306036576@qq.com>
2019-01-03 13:31:03 +0800

    Add category logic

--- src/renderer/main-page.ts
+++ src/renderer/main-page.ts
@@ -1,8 +1,61 @@
-import { ipcRenderer, Config } from "electron";
+import { ipcRenderer } from "electron";
 import { App, AppListConfig } from "../common";
 import { exec } from "child_process";
 
+const unHoverCategoryClassName = "un-hover-category";
+
+/** TODO: 换成 jq */
 let container = document.querySelector(".container");
+let categories = document.querySelector(".categories");
+let categoryNameInput = document.querySelector(
+  "#category-name"
+) as HTMLInputElement;
+
+$(".ui.dropdown").dropdown({
+  clearable: true
+});
+
+function hiddenModal() {
+  $(".ui.modal").modal("hide");
+}
+
+function showModal() {
+  $(".ui.modal").modal("show");
+}
+
+function traverseCategories(cb: (ele: Element) => void) {
+  let categories = document.querySelectorAll(".category");
+
+  categories.forEach(ele => cb(ele));
+}
+
+function createCategoryNode(content: string): Node | undefined {
+  if (content === "") {
+    alert("分组名称不能为空");
+  }
+
+  let isCreate = true;
+
+  traverseCategories(
+    ele =>
+      ele.textContent === content &&
+      ((isCreate = false) || alert("已有相同的分组"))
+  );
+
+  if (!isCreate) {
+    return;
+  }
+
+  let category = document
+    .querySelector(".main-category")!
+    .cloneNode(true) as HTMLDivElement;
+
+  category.textContent = content;
+  category.classList.remove("main-category");
+  category.classList.add(unHoverCategoryClassName);
+
+  return category;
+}
 
 function createAppHTMLElement({
   name,
@@ -40,7 +93,6 @@ function initDisplayApps(apps: AppListConfig, container: HTMLElement) {
 
   for (let key of Object.keys(apps)) {
     let app = apps[key];
-    console.info(app);
 
     container.appendChild(createAppHTMLElement(app));
   }
@@ -68,4 +120,36 @@ export function handlePageLoaded() {
   });
 
   ipcRenderer.send("get-apps");
+}
+
+export function handleCategoryClick(e: Event) {
+  let targetEle = e.target as HTMLDivElement;
+
+  traverseCategories(ele => {
+    if (targetEle === ele) {
+      targetEle.classList.remove(unHoverCategoryClassName);
+    } else {
+      ele.classList.add(unHoverCategoryClassName);
+    }
+  });
+}
+
+export function handleAddCategoryClick() {
+  categoryNameInput.value = "";
+  showModal();
+}
+
+export function handleAddCategoryOkClick() {
+  let ele = createCategoryNode(categoryNameInput.value || "");
+
+  if (ele === undefined) {
+    return;
+  }
+
+  categories!.appendChild(ele);
+  hiddenModal();
+}
+
+export function handleAddCategoryCancelClick() {
+  hiddenModal();
 }
