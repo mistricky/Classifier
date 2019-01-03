b82cac5394c2325f30a034f2dd11203b9dea4037
HaoDaWang <1306036576@qq.com>
2019-01-03 15:40:14 +0800

    Add events

---
+++ src/configs/event.ts
@@ -0,0 +1,9 @@
+export const enum Events {
+  CLOSE_TARGET_WINDOW = "close-target-window",
+  HIDDEN_TARGET_WINDOW = "hidden-target-window",
+  SAVE_APP_LIST = "save-app-list",
+  ADD_APP = "add-app",
+  GET_APPS = "get-apps",
+  ADD_CATEGORY = "add-category",
+  REMOVE_CATEGORY = "remove-category"
+}
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,3 +1,4 @@
 export * from "./window-config";
 export * from "./path-config";
 export * from "./auth";
+export * from "./event";
