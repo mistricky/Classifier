85da5c97795cf433e12677a5493e0f7486b5671e
HaoDaWang <1306036576@qq.com>
2019-01-03 15:45:46 +0800

    Use enum instead string

--- src/main/process-channel.ts
+++ src/main/process-channel.ts
@@ -1,36 +1,50 @@
 import { ipcMain, BrowserWindow, Event } from "electron";
 import { WindowsManager } from "../core";
-import { updateJSON, parseToConfig, AppManager } from "../common";
+import {
+  updateJSON,
+  parseToConfig,
+  AppManager,
+  CategoryManager
+} from "../common";
 import { APP_LIST } from "../configs";
+import { Events } from "../configs/event";
 
-ipcMain.on("close-target-window", (_e: Event, windowName: string) => {
+ipcMain.on(Events.CLOSE_TARGET_WINDOW, (_e: Event, windowName: string) => {
   WindowsManager.createInstance().closeWindow(windowName, true);
 });
 
-ipcMain.on("hidden-target-window", (_e: Event, windowName: string) => {
+ipcMain.on(Events.HIDDEN_TARGET_WINDOW, (_e: Event, windowName: string) => {
   (WindowsManager.createInstance().getWindow(
     windowName
   ) as BrowserWindow).hide();
 });
 
-ipcMain.on("save-app-list", () => {
+ipcMain.on(Events.SAVE_APP_LIST, () => {
   updateJSON(
     APP_LIST,
     JSON.stringify(parseToConfig(AppManager.createInstance().apps))
   );
 });
 
-ipcMain.on("add-app", async (e: Event, path: string) => {
+ipcMain.on(Events.ADD_APP, async (e: Event, path: string) => {
   let app = await AppManager.createInstance().addApp(path);
 
   e.sender.send("add-app-reply", app);
 });
 
-ipcMain.on("get-apps", (e: Event) => {
+ipcMain.on(Events.GET_APPS, (e: Event) => {
   e.sender.send(
     "get-apps-reply",
     parseToConfig(AppManager.createInstance().apps)
   );
+});
+
+ipcMain.on(Events.ADD_CATEGORY, (_e: Event, categoryName: string) => {
+  CategoryManager.createInstance().addCategory(categoryName);
+});
+
+ipcMain.on(Events.REMOVE_CATEGORY, (_e: Event, categoryName: string) => {
+  CategoryManager.createInstance().removeCategory(categoryName);
 });
 
 export {};
