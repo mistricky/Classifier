1adb0110f9169674d0733cb0ce5cb0feaa26232a
HaoDaWang <1306036576@qq.com>
2019-01-05 22:51:27 +0800

    Add UpdateSizePayload

--- src/main/process-channel.ts
+++ src/main/process-channel.ts
@@ -4,10 +4,17 @@ import {
   updateJSON,
   parseToConfig,
   AppManager,
-  CategoryManager
+  CategoryManager,
+  exportConfig
 } from "../common";
 import { APP_LIST } from "../configs";
 import { Events } from "../configs/event";
+
+export interface UpdateSizePayload {
+  windowName: string;
+  width: number;
+  height: number;
+}
 
 ipcMain.on(Events.CLOSE_TARGET_WINDOW, (_e: Event, windowName: string) => {
   WindowsManager.createInstance().closeWindow(windowName, true);
@@ -34,6 +41,10 @@ ipcMain.on(Events.ADD_APP, async (e: Event, path: string, category: string) => {
   e.sender.send(Events.ADD_APP_REPLY, replyMessage);
 });
 
+ipcMain.on(Events.GET_APPS_SYNC, (e: Event) => {
+  e.returnValue = AppManager.createInstance().apps;
+});
+
 ipcMain.on(Events.GET_APPS, (e: Event) => {
   e.sender.send(
     "get-apps-reply",
@@ -55,5 +66,18 @@ ipcMain.on(Events.GET_CATEGORIES, (e: Event) => {
     CategoryManager.createInstance().categories
   );
 });
+
+ipcMain.on(Events.EXPORT_CONFIG, async (_e: Event, filename: string) => {
+  await updateJSON(
+    APP_LIST,
+    JSON.stringify(parseToConfig(AppManager.createInstance().apps))
+  );
+  await updateJSON(filename, await exportConfig(), "/");
+});
+
+// ipcMain.on(Events.UPDATE_SIZE,(e:Event, payload:UpdateSizePayload) =>{
+//   let win = WindowsManager.createInstance()
+//   console.info()
+// });
 
 export {};
