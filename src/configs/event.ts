c8210c0d47a7b926010662fc773c004baae3155f
HaoDaWang <1306036576@qq.com>
2019-01-05 22:51:15 +0800

    Add more event

--- src/configs/event.ts
+++ src/configs/event.ts
@@ -4,10 +4,13 @@ export const enum Events {
   SAVE_APP_LIST = "save-app-list",
   ADD_APP = "add-app",
   GET_APPS = "get-apps",
+  GET_APPS_SYNC = "get-apps-sync",
   ADD_CATEGORY = "add-category",
   REMOVE_CATEGORY = "remove-category",
   ADD_APP_REPLY = "add-app-reply",
   GET_APPS_REPLY = "get-apps-reply",
   GET_CATEGORIES = "get-categories",
-  GET_CATEGORIES_REPLY = "get-categories-reply"
+  GET_CATEGORIES_REPLY = "get-categories-reply",
+  EXPORT_CONFIG = "save-config",
+  UPDATE_SIZE = "update-size"
 }
