24f6fd267affa8c3beb2e2141249498a1aed21bc
HaoDaWang <1306036576@qq.com>
2019-01-05 15:58:56 +0800

    Add more escape function

--- src/util/escape.ts
+++ src/util/escape.ts
@@ -1,3 +1,11 @@
 export function escapeSpace(str: string): string {
-  return str.replace(/(\s)/g, "\\ ");
+  return str.replace(/\s/g, "\\ ");
+}
+
+export function escapePoint(str: string): string {
+  return str.replace(/\./g, "\\.");
+}
+
+export function escapeAll(str: string): string {
+  return escapePoint(escapeSpace(str));
 }
